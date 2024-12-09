8a713b1877940c5d6e2a90af6790aa150acbd50d
ronaldmoura1989 <ronaldmoura1989@gmail.com>
2024-12-09 07:50:33 -0300

    modulo 2 completo

--- nao_upar/modulo2_importacao_dados.R
+++ nao_upar/modulo2_importacao_dados.R
@@ -12,7 +12,7 @@ diabetes = read_tsv("datasets/healthcare_diabetes_dataset/Healthcare-Diabetes.ts
 #read_delim(): função mais generalista
 coracao = read_delim("datasets/heart_disease_predition_dataset/dataset_heart.csv")
 
-####importação de planilhas do excel####
+#### importação de planilhas do excel####
 library(readxl)
 
 #escolher a planilha pelo numero
@@ -32,9 +32,20 @@ path |>
   imap(~ assign(.y, 
                 .x, envir = .GlobalEnv))
 
-#### Importação a partir do github #####
+#### importação a partir de URL. Ex.: github #####
+rice = read_delim("https://raw.githubusercontent.com/bio-hub/curso_R1/refs/heads/main/datasets/rice_pests_diseases/RICE.csv?token=GHSAT0AAAAAACW5UN5QUZG55EOJ53JO6CEOZ2V4H3Q")
 
+#### importação a partir do Google Sheets #####
+library(googlesheets4)
 
+#requisitar autorizacao
+gs4_auth()
 
+#baixar a planilha
+exames = read_sheet("https://docs.google.com/spreadsheets/d/1lj6w0owt2fXAoH03uKqQT0GxiecWu6TZoj7pMcVO3Ek/edit?gid=0#gid=0", sheet = 1)
+
+#### importação de datasets grandes ####
+library(data.table)
+meteoritos = fread("datasets/meteorite_landings/meteorite-landings.csv")
 ####FIM####
 
